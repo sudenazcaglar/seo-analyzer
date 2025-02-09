1edeac11952b810fd58eae0ee725d062e6529fbd
sudenazcaglar <ce@sudenazcaglar.com>
2025-02-09 13:58:04 +0300

    Add user auth endpoints

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import hello_world
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import hello_world, RegisterView, ProfileView
+
 
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
 
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "Hello from the SEO Analyzer!"})
+
+# Kayıt (Register) için View
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny] # Herkes kayıt olabilir.
+    serializer_class = RegisterSerializer
+
+
+# Profil görüntüleme için View
+class ProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated] # Giriş yapmadan erişilemez.
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
 
